1f87b145a58f399ace48ea191eec6235919e0195
Antonysibi <87759135+Antonysibi@users.noreply.github.com>
2021-08-26 16:29:32 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,68 +1,65 @@
 var garden,rabbit;
 var gardenImg,rabbitImg;
-var apple,appleImg
+var apple,appleImg 
 var leaf,leafImg
-var select_sprites = Math.round(random(1,2));
+
 
 function preload(){
   gardenImg = loadImage("garden.png");
-  rabbitImg = loadImage("rabbit.png");
+  rabbitImg = loadImage("rabbit.png");  
   appleImg = loadImage("apple.png");
-  leafImg = loadImage("leaf.png");
-}
-
-function setup(){
-  
+  leafImg = loadImage("leaf.png")
+ }
+function setup(){ 
   createCanvas(400,400);
-  
-// Moving background
-garden=createSprite(200,200);
-garden.addImage(gardenImg);
 
-//creating boy running
-rabbit = createSprite(180,340,30,30);
-rabbit.scale =0.09;
-rabbit.addImage(rabbitImg);
+  garden = createSprite(200,200);
+  garden.addImage(gardenImg);
 
-var rand = Math.round(random(1,100))
-console.log(rand);
+  rabbit = createSprite(180,340,30,30);
+  rabbit.scale =0.09;
+  rabbit.addImage(rabbitImg);
 }
 
 
 function draw() {
   background(0);
- rabbit.x = World.mouseX
-console.log(frameCount);
-createLeaves();
-createApples();
+  
+  rabbit.x = World.mouseX
+  
 
+  createLeaves();
+  createApples();
 
- drawSprites();
-}
+  var select_apples = Math.round(random,(1,2));
+  var select_leaves = Math.round(random,(1,2));
 
-function createApples(){
-  if(frameCount%80===0){
-    apple = createSprite(random(50,350),40,10,10);
-    apple.addAnimation(appleImg)
-    apple.scale = 1
-    apple.velocityY = 3
+  if (World.frameCount % 80 == 0) {
+    if (select_apples == 1){
+       createApples()
+    }
+  }    
+
+  if(World.frameCount % 80 == 0){
+    if(select_leaves == 1){
+       createLeaves()
+    }
   }
+  
+  drawSprites()
 }
 
 function createLeaves(){
-  if(frameCount%80===0){
-    leaf = createSprite(random(50,350),40,10,10);
-    leaf.addAnimation(leafImg);
-    leaf.scale = 1;
-    leaf.velocityY = 3;
-  }
+  var leaf = createSprite(200,Math.round(random(50, 350)),40, 10, 10);
+  leaf.addImage(leafImg);
+  leaf.velocityY = 3
+  leaf.scale = 0.05
 }
 
-if(frameCount%80===0){
-  if(select_sprites===1){
-    function createApples()
-  }
-  else{
-    function createLeaves()
-  }
+function createApples(){
+  var apple = createSprite(200,Math.round(random(50, 350)),40, 10, 10);
+  apple.addImage(appleImg);
+  apple.velocityY = 3
+  apple.scale = 0.05
 }
+
