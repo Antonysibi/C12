57c8c44bb119d29cf59886d164f9eeaa31d258d0
Antonysibi <87759135+Antonysibi@users.noreply.github.com>
2021-09-17 08:04:10 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -10,15 +10,20 @@ function preload(){
   appleImg = loadImage("apple.png");
   leafImg = loadImage("leaf.png")
  }
+
 function setup(){ 
   createCanvas(400,400);
 
   garden = createSprite(200,200);
   garden.addImage(gardenImg);
 
-  rabbit = createSprite(180,340,30,30);
-  rabbit.scale =0.09;
-  rabbit.addImage(rabbitImg);
+ // rabbit = createSprite(180,340,30,30);
+  //rabbit.scale =0.09;
+  //rabbit.addImage(rabbitImg);
+
+  //leaf = createSprite(180,340,30,30);
+  //leaf.scale = 0.09;
+  //leaf.addImage(leafImg)
 }
 
 
@@ -31,37 +36,41 @@ function draw() {
   createLeaves();
   createApples();
 
-  var select_apples = Math.round(random,(1, 3));
-  var select_leaves = Math.round(random,(1, 3));
+  var select_apples = Math.round(random(1, 3));
+  var select_leaves = Math.round(random(1,3));
   
 
 
   if (World.frameCount % 80 == 0) {
     if (select_apples == 1){
        createApples()
+       select_apples = Math.round(random(1,3))
     }
   }    
 
-  if(World.frameCount % 60 == 0){
+  if(World.frameCount % 80 == 0){
     if(select_leaves == 2){
        createLeaves()
+       select_apples = Math.round(random(1,3))
     }
   }
-  
-  drawSprites()
-}
 
-function createLeaves(){
-  var leaf = createSprite(Math.round(random(50, 350)),40, 10, 10);
+    drawSprites()
+  }
+
+    function createLeaves(){
+  var leaf = createSprite(random(1,3),40, 10, 10);
   leaf.addImage(leafImg);
   leaf.velocityY = 3
   leaf.scale = 0.05
 }
 
 function createApples(){
-  var apple = createSprite(Math.round(random(50, 350)),40, 10, 10);
+  var apple = createSprite(random(1,3),40, 10, 10);
   apple.addImage(appleImg);
   apple.velocityY = 3
   apple.scale = 0.05
 }
+
+
 
