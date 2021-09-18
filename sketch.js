cdc246299bdd66d207240623c159f5b60911fb7d
Antonysibi <87759135+Antonysibi@users.noreply.github.com>
2021-09-18 15:23:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
-var garden,rabbit;
-var gardenImg,rabbitImg;
-var apple,appleImg 
-var leaf,leafImg
+  var garden,rabbit;
+  var gardenImg,rabbitImg;
+  var apple,appleImg 
+  var leaf,leafImg
 
 
 function preload(){
@@ -17,13 +17,10 @@ function setup(){
   garden = createSprite(200,200);
   garden.addImage(gardenImg);
 
- // rabbit = createSprite(180,340,30,30);
-  //rabbit.scale =0.09;
-  //rabbit.addImage(rabbitImg);
+  rabbit = createSprite(180,340,30,30);
+  rabbit.scale =0.09;
+  rabbit.addImage(rabbitImg);
 
-  //leaf = createSprite(180,340,30,30);
-  //leaf.scale = 0.09;
-  //leaf.addImage(leafImg)
 }
 
 
@@ -36,40 +33,38 @@ function draw() {
   createLeaves();
   createApples();
 
-  var select_apples = Math.round(random(1, 3));
-  var select_leaves = Math.round(random(1,3));
+  var select_apples = Math.round(random(50, 350));
+  var select_leaves = Math.round(random(50,350));
   
 
 
   if (World.frameCount % 80 == 0) {
-    if (select_apples == 1){
-       createApples()
-       select_apples = Math.round(random(1,3))
+    if(select_apples == 1){
+      createApples()
+    }
+    else{
+      createLeaves()
     }
   }    
 
-  if(World.frameCount % 80 == 0){
-    if(select_leaves == 2){
-       createLeaves()
-       select_apples = Math.round(random(1,3))
-    }
+
+  drawSprites()
   }
 
-    drawSprites()
-  }
-
-    function createLeaves(){
-  var leaf = createSprite(random(1,3),40, 10, 10);
+function createLeaves(){
+  leaf = createSprite(random(50,3500),40,10,10);
   leaf.addImage(leafImg);
-  leaf.velocityY = 3
   leaf.scale = 0.05
+  leaf.velocityY = 3
+  leaf.lifetime = 90
 }
 
 function createApples(){
-  var apple = createSprite(random(1,3),40, 10, 10);
+  apple = createSprite(random(5,3500),40,10,10);
   apple.addImage(appleImg);
-  apple.velocityY = 3
   apple.scale = 0.05
+  apple.velocityY = 3
+  apple.lifetime = 90
 }
 
 
